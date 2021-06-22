18f71f7cde2f4814b7ec9abfc47be1adac07b590
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 10:48:13 -0700

    Initial Commit

---
+++ form-control/form_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  form_controlApp.swift
+//  form-control
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct form_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
